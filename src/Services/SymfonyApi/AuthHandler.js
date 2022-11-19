bd6529355d241bd0b5e95e504a14fe1451eca32a
vclong2003 <vclong2003@gmail.com>
2022-11-20 05:07:25 +0700

    bk

--- src/Services/SymfonyApi/AuthHandler.js
+++ src/Services/SymfonyApi/AuthHandler.js
@@ -57,3 +57,29 @@ export function logout() {
   localStorage.clear();
   window.location.href = "/";
 }
+
+export function getRole(callback) {
+  if (localStorage.getItem("sessionId")) {
+    fetch("https://127.0.0.1:8000/api/auth/role", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        sessionId: localStorage.getItem("sessionId"),
+      },
+    })
+      .then((response) => {
+        // 202: verified, 406: not verified
+        if (response.status === 202) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          callback(data.role);
+        }
+      })
+      .catch((err) => {
+        console.log("Error: " + err);
+      });
+  }
+}
--- src/Services/SymfonyApi/UserHandler.js
+++ src/Services/SymfonyApi/UserHandler.js
@@ -1,26 +1,2 @@
-export function getRole(callback) {
-    if (localStorage.getItem("sessionId")) {
-      fetch("https://127.0.0.1:8000/api/user/role", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          sessionId: localStorage.getItem("sessionId"),
-        },
-      })
-        .then((response) => {
-          // 202: verified, 406: not verified
-          if (response.status === 202) {
-            return response.json();
-          }
-        })
-        .then((data) => {
-          if (data) {
-            callback(data.role);
-          }
-        })
-        .catch((err) => {
-          console.log("Error: " + err);
-        });
-    }
-  }
+
   
